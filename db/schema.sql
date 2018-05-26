51cbca4bbe42b52644e21ff437db6385c2ca9461
Joe Finny <finne132@umn.edu>
2018-05-26 11:08:08 -0500

    added pace marker and hole handicap to course table

--- db/schema.sql
+++ db/schema.sql
@@ -17,7 +17,9 @@ CREATE TABLE Course (
     dt1 INT NOT NULL,
     dt2 INT NOT NULL,
     dt3 INT NOT NULL,
-    mapurl VARCHAR(250)   
+    mapurl VARCHAR(250),
+    pacemark INT NOT NULL,
+    holecap INT NOT NULL,   
 );
 
 CREATE TABLE Score (
