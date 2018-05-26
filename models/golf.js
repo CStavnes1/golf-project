ed129b9e738eb03a82eace15b46ea2b5825e91d1
ChrisStavnes <ChrisStavnes@gmail.com>
2018-05-26 12:27:10 -0500

    homepage flushed out

---
+++ models/golf.js
@@ -0,0 +1,4 @@
+// import orm.js into golf.js
+var orm = require('../config/orm.js');
+
+// create the code that will call the ORM functions
---
+++ server.js
@@ -0,0 +1,16 @@
+// Node Dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+ 
+
+app.listen(PORT, function () {
+
+    console.log("Server listening on: http://localhost:" + PORT);
+});
