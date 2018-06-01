f7b44ffcaecbbda9b1638b9ca08d0e362bb8dfb5
Joe Finny <finne132@umn.edu>
2018-05-31 20:53:01 -0500

    joe: all of today's work

--- controllers/golf_controllers.js
+++ controllers/golf_controllers.js
@@ -6,7 +6,19 @@ var golf = require('../models/golf.js');
 // Index redirect
 
 router.get('/', function (req, res) {
-    res.redirect('/index.html');;
+    res.render('index');
+});
+
+router.get('/player', function (req, res) {
+    res.render('player');
+});
+
+router.get('/hole', function (req, res) {
+    res.render('hole');
+});
+
+router.get('/history', function (req, res) {
+    res.render('history');
 });
 
 
